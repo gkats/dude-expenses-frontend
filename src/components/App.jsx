62192e9701bc8c0710fae33fc9994d1d715808f1
gkats <giannis.katsanos@gmail.com>
2017-02-12 13:03:03 +0200

    Added a custom theme

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import AppBar from 'material-ui/AppBar';
+import AppTheme from '../theme';
 import ExpensesContainer from './ExpensesContainer';
 
 const App = () => (
-  <MuiThemeProvider>
+  <MuiThemeProvider muiTheme={getMuiTheme(AppTheme)}>
     <div>
       <AppBar title="Dude, where's my expenses?" />
       <ExpensesContainer />
---
+++ src/theme.js
@@ -0,0 +1,7 @@
+import { orange500 } from 'material-ui/styles/colors';
+
+export default Object.freeze({
+  palette: {
+    primary1Color: orange500
+  }
+});

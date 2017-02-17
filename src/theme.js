d95df01ae6a840ef74b4863a82133280c6644ff1
gkats <giannis.katsanos@gmail.com>
2017-02-17 22:37:36 +0200

    Changes to app theme

--- src/theme.js
+++ src/theme.js
@@ -1,7 +1,13 @@
-import { orange500 } from 'material-ui/styles/colors';
+import { orange700, orangeA500, grey300 } from 'material-ui/styles/colors';
 
 export default Object.freeze({
   palette: {
-    primary1Color: orange500
+    primary1Color: orange700,
+    primary2Color: orangeA500,
+    primary3Color: grey300,
+    pickerHeaderColor: orange700
+  },
+  datePicker: {
+    selectColor: orange700
   }
 });
